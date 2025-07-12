b1cb478a3296dfddebfe9011dae7836252085d48
Jennifer Gopez <jegopez@gmail.com>
2025-07-12 12:56:19 +0800

    add playwright tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,9 @@
 // Playwright Basics Activity 1
 
+
 import { test, expect } from '@playwright/test';
+
+// Happy path, login with valid credentials
 
 test('Verify login with valid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/')
@@ -10,12 +13,34 @@ test('Verify login with valid credentials', async ({ page }) => {
   await expect(page.getByText('Swag Labs')).toBeVisible()
 });
 
+// Login with incorrect password
+
 test('Verify login fails with invalid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/')
   await page.locator('[data-test="username"]').fill('standard_user')
   await page.locator('[data-test="password"]').fill('wrong_password')
   await page.locator('[data-test="login-button"]').click()
-  await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match')
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service')
+});
+
+// Login with blank username
+
+test('Verify login fails with blank username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username is required');
+});
+
+// Login with blank password
+
+test('Verify login fails with blank password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Password is required');
 });
 
 // You can run this test via Headed or Headless mode
