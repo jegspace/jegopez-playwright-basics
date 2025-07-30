de7a4f34108501e7b0f11fc515eba55f7b3333fb
Jennifer Gopez <jegopez@gmail.com>
2025-07-30 23:56:20 +0800

    chore: update files for the continuation of session 12

--- tests/features/test-step.spec.ts
+++ tests/features/test-step.spec.ts
@@ -1,34 +1,49 @@
 // Session 12 test.step
+// Example use of annotation
+// Example use of single tagging of test - > { tag: '@Happy-Path'},
+// Example use of multiple tagging of test
 
 import { test, expect } from "@playwright/test";
 
-test.describe("Automation Exercise - Navigation", () => {
+test.describe("Automation Exercise - Navigation",
+  {
+    annotation: {
+      type: 'Functional Testing',
+      description: 'Annotate the Test Suite Test Step' },      
+  },  () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://automationexercise.com/");
   });
-  test.afterEach(async ({ page }) => {
-    await page.close();
-  });
 
   // Our test should be able to run in isolation
-  test("Should successfully visit product page", async ({ page }) => {
+  
+
+  test("Should successfully visit product page", { tag: '@Happy-Path'}, async ({ page }) => {
     // Step 1
     await test.step("Navigate to Products Page", async () => {
       await page.locator("a[href='/products']").click();
     });
     // Step 2
+    
     await test.step("Verify URL and All Products label", async () => {
       await expect(page).toHaveURL("https://automationexercise.com/products");
-      await expect(page.locator("h2.title.text-center")).toHaveText(
-        "All Products"
-      );
+      await expect(page.locator("h2.title.text-center")).toHaveText("All Products");
     });
   });
 
-  test("Verify all Products label", async ({ page }) => {
-    await page.locator("a[href='/products']").click();
-    await expect(
-      page.getByRole("heading", { name: "All Products" })
-    ).toContainText(/All Products/);
-  });
+  test(
+    "Should successfully Search Winter Top",
+    {
+      annotation: 
+        { type: "issue", description: "can't search the product - https://github.com/jegspace/jegopez-playwright-basics/issues/1#issue-3277441925"}, 
+      tag: ["@Smoke", "@Regression", "@Happy-Path"],       
+    },
+    async ({ page }) => {
+      await page.locator("a[href='/products']").click();
+      await page
+        .getByRole("textbox", { name: "Search Product" })
+        .fill("Winter Top");
+      await page.getByRole("button", { name: "" }).click();
+    }
+  );
 });
