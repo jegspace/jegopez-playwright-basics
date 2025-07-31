e763f029f723697bb5f59219806e1ccabe794507
Jennifer Gopez <jegopez@gmail.com>
2025-08-01 00:26:05 +0800

    Chore: Update files for cont. of Session 12

--- tests/features/test-step.spec.ts
+++ tests/features/test-step.spec.ts
@@ -5,45 +5,107 @@
 
 import { test, expect } from "@playwright/test";
 
-test.describe("Automation Exercise - Navigation",
+test.describe(
+  "Automation Exercise - Navigation",
   {
     annotation: {
-      type: 'Functional Testing',
-      description: 'Annotate the Test Suite Test Step' },      
-  },  () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("https://automationexercise.com/");
-  });
-
-  // Our test should be able to run in isolation
-  
-
-  test("Should successfully visit product page", { tag: '@Happy-Path'}, async ({ page }) => {
-    // Step 1
-    await test.step("Navigate to Products Page", async () => {
-      await page.locator("a[href='/products']").click();
-    });
-    // Step 2
-    
-    await test.step("Verify URL and All Products label", async () => {
-      await expect(page).toHaveURL("https://automationexercise.com/products");
-      await expect(page.locator("h2.title.text-center")).toHaveText("All Products");
-    });
-  });
-
-  test(
-    "Should successfully Search Winter Top",
-    {
-      annotation: 
-        { type: "issue", description: "can't search the product - https://github.com/jegspace/jegopez-playwright-basics/issues/1#issue-3277441925"}, 
-      tag: ["@Smoke", "@Regression", "@Happy-Path"],       
+      type: "Functional Testing",
+      description: "Annotate the Test Suite Test Step",
     },
-    async ({ page }) => {
-      await page.locator("a[href='/products']").click();
-      await page
-        .getByRole("textbox", { name: "Search Product" })
-        .fill("Winter Top");
-      await page.getByRole("button", { name: "" }).click();
-    }
-  );
-});
+  },
+  () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("https://automationexercise.com/");
+    });
+
+    // Our test should be able to run in isolation
+
+    test(
+      "Should successfully visit product page",
+      { tag: "@Happy-Path" },
+      async ({ page }) => {
+        // Step 1
+        await test.step("Navigate to Products Page", async () => {
+          await page.locator("a[href='/products']").click();
+        });
+        // Step 2
+
+        await test.step("Verify URL and All Products label", async () => {
+          await expect(page).toHaveURL(
+            "https://automationexercise.com/products"
+          );
+          await expect(page.locator("h2.title.text-center")).toHaveText(
+            "All Products"
+          );
+        });
+      }
+    );
+
+    test(
+      "Should successfully Search Winter Top",
+      {
+        annotation: {
+          type: "issue",
+          description:
+            "can't search the product - https://github.com/jegspace/jegopez-playwright-basics/issues/1#issue-3277441925",
+        },
+        tag: ["@Smoke", "@Regression", "@Happy-Path"],
+      },
+      async ({ page }) => {
+        await page.locator("a[href='/products']").click();
+        await page
+          .getByRole("textbox", { name: "Search Product" })
+          .fill("Winter Top");
+        await page.getByRole("button", { name: "" }).click();
+      }
+    );
+
+    // test.slow();
+    test(
+      "Should successfully Search Sleeveless Dress",
+      {
+        annotation: {
+          type: "issue",
+          description:
+            "can't search the product - https://github.com/jegspace/jegopez-playwright-basics/issues/1#issue-3277441925",
+        },
+        tag: "@Functional-Testing",
+      },
+
+      async ({ page, browserName }) => {
+        test.slow(
+          browserName === "chromium",
+          "This feature is slow in Chromium"
+        );
+        await page.locator("a[href='/products']").click();
+        await page
+          .getByRole("textbox", { name: "Search Product" })
+          .fill("Sleeveless Dress");
+        await page.getByRole("button", { name: "" }).click();
+      }
+    );
+
+    // test.skip ; test.only ; test.fixme
+    test.fixme(
+      "To be fixed - Should successfully Search Blue Top",
+      {
+        annotation: {
+          type: "issue",
+          description:
+            "can't search the product - https://github.com/jegspace/jegopez-playwright-basics/issues/1#issue-3277441925",
+        },
+        tag: "@Functional-Testing",
+      },
+
+      async ({ page, browserName }) => {
+        test.slow(
+          browserName === "chromium",
+          "This feature is slow in Chromium"
+        );
+        await page.locator("a[href='/products']").click();
+        await page.getByRole('textbox', { name: 'Search Product' }).fill('Blue Top');
+        await page.getByRole('button', { name: '' }).click();
+      }
+    );
+
+  });
